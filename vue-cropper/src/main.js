d370f981e0896a6b65b381f100e53f919b70fc35
Hoang Minh Hieu <hmh982212@gmail.com>
2019-12-23 21:09:56 +0700

    fb

---
+++ vue-cropper/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+import router from './router'
+import store from './store'
+import VueCropper from 'vue-cropperjs';
+import 'cropperjs/dist/cropper.css';
+
+Vue.component(VueCropper);
+Vue.use(ElementUI);
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
